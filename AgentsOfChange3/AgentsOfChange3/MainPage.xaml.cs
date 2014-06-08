6f878e4a66300429960603eb78d569d34392f0f5
cjei1225 <jeijei1225@gmail.com>
2014-06-08 15:38:01 +0800

    1st upload
    Upload for the 1st time

---
+++ AgentsOfChange3/AgentsOfChange3/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace AgentsOfChange3
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void textblock_Mission_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Mission_PivotPage.xaml", UriKind.Relative));
+        }
+
+        private void Panorama_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count < 1) return;
+            if (!(e.AddedItems[0] is PanoramaItem)) return;
+
+            PanoramaItem selectedItem = (PanoramaItem)e.AddedItems[0];
+
+            string strTag = (string)selectedItem.Tag;
+            if (strTag.Equals("events"))
+            {
+                this.ApplicationBar.Mode = ApplicationBarMode.Default;
+            }
+            
+            else //if (strTag.Equals("mission"))
+            {
+                this.ApplicationBar.Mode = ApplicationBarMode.Minimized;
+            }
+            
+           
+
+        }
+
+        private void stackpanel_TodaysMission_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Mission_PivotPage.xaml", UriKind.Relative));
+        }
+
+        private void AppBarMI_Settings_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AboutPage.xaml",UriKind.Relative));
+        }
+
+        private void AppBarIB_MoreEvents_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/BrowseAgenada_PivotPage.xaml", UriKind.Relative));
+        }
+
+      
+    }
+}
---
+++ AgentsOfChange3/AgentsOfChange3/Mission_PivotPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace AgentsOfChange3
+{
+    public partial class Mission_PivotPage : PhoneApplicationPage
+    {
+        private string user = "USER";
+        private string mission = "MISSION";
+
+        public string Mission
+        {
+            get { return mission; }
+            set { mission = value; }
+        }
+
+        public string User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+
+        public Mission_PivotPage()
+        {
+            InitializeComponent();
+            //textblock_yourMission.Text = "Your mission agent " + User + ", should you choose to accept it, " + mission;
+        }
+    }
+}
